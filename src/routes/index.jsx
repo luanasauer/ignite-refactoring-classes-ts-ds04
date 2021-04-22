54c907e49dbba5140be3404f9f4370ff40ee4d35
Luana Sauer <luanasauer23@gmail.com>
2021-04-22 18:12:19 -0300

    projeto finalizado

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import { Switch, Route } from 'react-router-dom';
+import { Dashboard } from '../pages/Dashboard';
 
-import Dashboard from '../pages/Dashboard';
 
-const Routes = () => (
-  <Switch>
-    <Route path="/" exact component={Dashboard} />
-  </Switch>
-);
+export function Routes() {
 
-export default Routes;
+  return (
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+    </Switch>
+  );
+}
+
